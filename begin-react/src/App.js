2cf19a200d17833c37147185d64704cae2f1e648
kenta5293 <kenta.03.1110@gmail.com>
2021-07-20 16:31:37 +0900

    Begin-React : useRef to Create Variable in Component

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import UserList from './components/UserList';
 import './App.css';
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  }
+
   return (
     <>
-      <UserList />
+      <UserList users={users} />
     </>
   );
 }
