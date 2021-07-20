c8c6ec1f8babba672c726d5f9c4adb5b4e036f10
kenta5293 <kenta.03.1110@gmail.com>
2021-07-20 16:42:10 +0900

    Begin-React : Add items to Array

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,9 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './components/UserList';
+import CreateUser from './components/CreateUser';
 import './App.css';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -19,18 +35,35 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
-    // 나중에 구현 할 배열에 항목 추가하는 로직
-    // ...
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+    // 다른 방법으로는 concat을 이용하는 방법이 있다
+    // concat 함수는 기존의 배열을 수정하지 않고, 새로운 원소가 추가된 새로운 배열을 만들어 준다
+    // setUsers(users.concat(user));
 
+    setInputs({
+      username: '',
+      email: ''
+    });
     nextId.current += 1;
   }
 
   return (
     <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} />
     </>
   );
